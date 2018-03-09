c9fb64d4443de78ae4e62ddbc618ad2293c19b77
tabreturn <github@tabreturn.com>
2018-03-10 11:13:39 +1300

    - setup fonts and visual log

--- README.md
+++ README.md
@@ -4,13 +4,13 @@
 
 In this game, your job is to save Mr. Yum (or, Tom, to his friends) from drowning in your soup. By solving a series of puzzles, you'll be rewarded with a top-up in your soup bowl. Once the soup reaches the brim, Mr. Yum can reach for the edge and climb out.
 
-Artist: Mary Guo
+Artist: Mary Guo  
 
 Made with [löve](https://love2d.org/) <3
+Fonts: Cute Font by Paula Tennet; Quicksand be Andrew Paglinawan
 
 Open Jam Aotearoa 2018 entry
 (Game Jam theme: "soup")
-
 
 ## To-Do
 
--- game.love/main.lua
+++ game.love/main.lua
@@ -1,8 +1,22 @@
 function love.load()
+  
+  -- window setup
   love.window.setTitle('Dining at Lindy\'s')
-  love.window.setMode(960, 540, {highdpi=true})
-  love.graphics.setBackgroundColor(255, 255, 255) -- white bg
+  winwidth   = 1920
+  winheight  = 1080
+  winmargin  = 50
+  rm         = 2 --highdpi multiplier
+  love.window.setMode(winwidth/rm, winheight/rm, {highdpi=true})
+  love.graphics.setBackgroundColor(255, 255, 255)
+  
+  -- assets
+  fontcute = love.graphics.newFont('fonts/cute.ttf', 100)
+  fontread = love.graphics.newFont('fonts/quicksand.ttf', 35)
   placeholder = love.graphics.newImage('ingredients/placeholder.png')
+  
+  -- stage variables
+  level = 0
+  
 end
 
 
@@ -11,10 +25,23 @@ function love.update(dt)
 end
 
 function love.draw()
-  -- love.graphics.draw(drawable, x, y, r, sx, sy, ox, oy, kx, ky)
-  love.graphics.draw(placeholder, 0, 0, 0, 0.5, 0.5)
   
+  love.graphics.draw(placeholder, 0, 0, 0, 0.5, 0.5) --love.graphics.draw(drawable, x, y, r, sx, sy, ox, oy, kx, ky)
+  
+  if 0 == 0 then
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.setFont(fontcute)
+    love.graphics.print(level, winwidth-fontcute:getWidth(level)-winmargin, 0+winmargin)
+  end
+  
+  -- quit game
   if love.keyboard.isDown('q') then
     love.event.quit(0)
   end
+  
+  -- debug log
+  log = 'stuff ...'
+  love.graphics.setFont(fontread)
+  love.graphics.print(log, 0, winheight-100)
+  
 end
