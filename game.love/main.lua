f903bf6aa482a49b34f02d5a772bb6301e9cce16
tabreturn <github@tabreturn.com>
2018-03-10 11:38:46 +1300

    - add placeholder images

--- game.love/main.lua
+++ game.love/main.lua
@@ -12,10 +12,13 @@ function love.load()
   -- assets
   fontcute = love.graphics.newFont('fonts/cute.ttf', 100)
   fontread = love.graphics.newFont('fonts/quicksand.ttf', 35)
-  placeholder = love.graphics.newImage('ingredients/placeholder.png')
+  ingapple = love.graphics.newImage('ingredients/apple.png')
+  ingcarrot = love.graphics.newImage('ingredients/apple.png')
+  ingtofu = love.graphics.newImage('ingredients/tofu.png')
   
   -- stage variables
   level = 0
+  log = ''
   
 end
 
@@ -26,7 +29,8 @@ end
 
 function love.draw()
   
-  love.graphics.draw(placeholder, 0, 0, 0, 0.5, 0.5) --love.graphics.draw(drawable, x, y, r, sx, sy, ox, oy, kx, ky)
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.draw(ingapple, 0, 0, 0.5, 0.5) --love.graphics.draw(drawable, x, y, r, sx, sy, ox, oy, kx, ky)
   
   if 0 == 0 then
     love.graphics.setColor(0, 0, 0)
@@ -34,13 +38,22 @@ function love.draw()
     love.graphics.print(level, winwidth-fontcute:getWidth(level)-winmargin, 0+winmargin)
   end
   
+  -- selections
+  if love.keyboard.isDown('z') then
+    log = 'z'
+  elseif love.keyboard.isDown('x') then
+    log = 'x'
+  elseif love.keyboard.isDown('c') then
+    log = 'c'
+  end
+  
+  
   -- quit game
   if love.keyboard.isDown('q') then
     love.event.quit(0)
   end
   
   -- debug log
-  log = 'stuff ...'
   love.graphics.setFont(fontread)
   love.graphics.print(log, 0, winheight-100)
   
